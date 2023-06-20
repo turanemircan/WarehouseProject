db548d13f73379a22abc632e7fc4cc267e620d71
alkeskn85 <alkeskn85@gmail.com>
2023-06-21 00:25:28 +0200

    V3

--- src/main/java/project01/Urunler.java
+++ src/main/java/project01/Urunler.java
@@ -12,6 +12,45 @@ public class Urunler {
 
     }
 
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProducter() {
+        return producter;
+    }
+
+    public void setProducter(String producter) {
+        this.producter = producter;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getShelf() {
+        return shelf;
+    }
+
+    public void setShelf(String shelf) {
+        this.shelf = shelf;
+    }
 
     public Urunler(String productName, String producter, int amount, String unit, String shelf) {
         this.productName = productName;
